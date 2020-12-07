b4a608ee48edc5bc6863b47382e5088006f346d8
philcronin <philcronin20@gmail.com>
2020-12-07 14:39:38 -0500

    worked on getting tool images to show

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -7,7 +7,10 @@ import { Injectable } from '@angular/core';
 export class RecipeService {
   searchURL: string = 'https://api.spoonacular.com/recipes/complexSearch';
   ingredientURL: string = 'https://api.spoonacular.com/recipes/id/information';
-  apiKey: string = '7ed03fd3e48b465985205b580d3d8763';
+  // apiKey: string = '7ed03fd3e48b465985205b580d3d8763'; // Phil
+  apiKey: string = '1e0e6ac413064d44ad1312d4c942f92a'; // Leslie
+  // apiKey: string = 'ed4e188160314292ab3ffe3a174eec84'; // Kyle
+  // apiKey: string = '64b2bbdba0214d90aa607e25138b53e8'; // Nicole
 
   constructor(private http: HttpClient) {}
 
