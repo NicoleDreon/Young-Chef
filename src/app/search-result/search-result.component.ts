8ea12888e8e83c4a673e4b79b2b1e272fdf6f69e
Kyle <xikkzz@gmail.com>
2020-12-08 14:44:16 -0500

    can favorite from search results page

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RecipeBox } from '../interfaces/recipe-box';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-search-result',
@@ -8,8 +10,9 @@ import { Router } from '@angular/router';
 })
 export class SearchResultComponent implements OnInit {
   @Input() recipe: any;
-
-  constructor(private router: Router) {}
+  // @Input() recipeInfo: any | null;
+  // @Input() uniqueTools: any[] = [];
+  constructor(private router: Router, private recipeService: RecipeService) {}
 
   ngOnInit(): void {}
 
@@ -20,5 +23,10 @@ export class SearchResultComponent implements OnInit {
       },
     });
     console.log(id);
+  };
+
+  //uses service 'editFavorites' to place recipe into favoritesz
+  toggleFavorites = () => {
+    this.recipeService.editFavorites(this.recipe);
   };
 }
