542c25540757df2c055824ec17e54c97a9348dee
philcronin <philcronin20@gmail.com>
2020-12-15 10:40:03 -0500

    added template for basics page

--- src/app/the-basics/the-basics.component.ts
+++ src/app/the-basics/the-basics.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-the-basics',
   templateUrl: './the-basics.component.html',
-  styleUrls: ['./the-basics.component.css']
+  styleUrls: ['./the-basics.component.css'],
 })
 export class TheBasicsComponent implements OnInit {
+  showIndex: number = 1;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  setShowIndex = (index: number) => {
+    this.showIndex = index;
+  };
 }
